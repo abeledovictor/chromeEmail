031d44df1c2e649514500f066bffcae77cdc3a53
victor <abeledovictor@gmail.com>
2018-07-07 17:45:00 -0300

    move credentials to a different file. add credentials.py to gitignore

--- chromeEmail/gmailScript.py
+++ chromeEmail/gmailScript.py
@@ -1,12 +1,10 @@
-
+import credentials
 from settings import Settings
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-mail_address = ''
-password = ''
 
 chromedriver_location = Settings.chromedriver_location
 
@@ -15,7 +13,7 @@ driver = webdriver.Chrome(chromedriver_location)
 url = 'https://www.google.com/accounts/Login?hl=ja&continue=http://www.google.co.jp/'
 driver.get(url)
 
-driver.find_element_by_id("identifierId").send_keys(mail_address)
+driver.find_element_by_id("identifierId").send_keys(credentials.email)
 driver.find_element_by_id("identifierNext").click()
 WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "password")))
-driver.find_element_by_name("password").send_keys(password)
+driver.find_element_by_name("password").send_keys(credentials.password)
